9b7628a6c03251f5643828d3622d8dc31cedede5
mero93m <merabkopaleishvili1993@gmail.com>
2026-04-11 02:13:38 +0400

    feat: add button variants

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" className={`${inter.variable} light`} style={{ colorScheme: 'light' }}>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );

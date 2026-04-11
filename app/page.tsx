e004c7de205b3f77783c4d9858abb2fdbe9ac685
mero93m <merabkopaleishvili1993@gmail.com>
2026-04-11 21:43:04 +0400

    feat: add icons registry and Icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { Icon, IconPathRegistry, isIconName } from '@/components/ui/Icon';
 
 export default function Home() {
+  const icons = Object.keys(IconPathRegistry);
+
+  console.log('icons', icons.length, icons);
+
   return (
     <main className="flex w-full flex-1 flex-col items-center justify-center gap-5 p-20">
       <div className="flex gap-5">
@@ -16,6 +21,9 @@ export default function Home() {
         <Button variant="ghost" disabled>
           Ghost
         </Button>
+      </div>
+      <div className="flex flex-wrap gap-1">
+        {icons.map((icon) => isIconName(icon) && <Icon name={icon} key={icon} className={'text-greyscale-700'}></Icon>)}
       </div>
     </main>
   );
